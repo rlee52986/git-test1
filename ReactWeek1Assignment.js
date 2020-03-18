8361d5c4ab7b5b87e8d751af5247165f917a0519
Rocky Lee <rlee52986@gmail.com>
2020-03-17 20:09:56 -0500

    First commit

---
+++ ReactWeek1Assignment.js
@@ -0,0 +1,39 @@
+class Student {
+    constructor(name, email, community) {
+        this.name = name;
+        this.email = email;
+        this.community = community;
+    }
+}
+
+class Bootcamp {
+    constructor(name, level, students = []) {
+        this.name = name;
+        this.level = level;
+        this.students = students;
+    }
+
+    registerStudent(studentToRegister) {
+        if students.filter(student => student.includes(studentToRegister));
+        {
+            console.log("This student is already registered.")
+        }
+        else {
+            this.students.push(studentToRegister);
+        }
+        {
+            console.log(`Registering ${email} to the bootcamp")
+}
+        return this.students;
+}
+
+
+
+
+
+
+
+
+
+
+
